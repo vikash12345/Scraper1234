c8ed1485cb93a10774e56f50ad11f749be48b703
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-31 12:18:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -33,7 +33,8 @@ $cHeadres = array(
 	   
  
  
-$Links	=	array('https://www.redfin.com/county/536/GA/Cobb-County/filter/include=sold-1wk');
+$Links	=	array('https://www.redfin.com/county/536/GA/Cobb-County/filter/include=sold-1wk',
+		     'https://www.redfin.com/county/534/GA/Clayton-County/filter/include=sold-1wk');
 
 for ($mainpage = 0; $mainpage < sizeof($Links); $mainpage++)
 {
