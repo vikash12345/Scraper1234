bf291cb8d09f9f92b813f26252f575dd61db5d33
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-31 11:47:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -54,10 +54,10 @@ for ($mainpage = 0; $mainpage < sizeof($Links); $mainpage++)
 			for($j = 0; $j <= $num; $j++) 
 				{
 				
-					$sold 			=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/div[2]/span",0);
-					$address		=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/a[2]/div[1]/div[2]",0);
-					$listingurl		=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[2]/div[2]/a",0);
-					$price			=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/a[2]/div[1]/div[1]/span[2]",0);
+					$sold 			=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/div[2]/span",0)->plaintext;
+					$address		=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/a[2]/div[1]/div[2]",0)->plaintext;;
+					$listingurl		=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[2]/div[2]/a",0)->href;
+					$price			=	$pages->find("//*[@id='MapHomeCard_$j']/div/div[1]/a[2]/div[1]/div[1]/span[2]",0)->plaintext;;
 					
 							
 					if($price != '' || $price != null)
